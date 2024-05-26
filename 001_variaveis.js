cfe6de0bf5c7705c5cb28df74b21498e26127596
escuderodev <escudero.ti@gmail.com>
2024-05-26 11:51:21 -0300

    feat: iniciando os estudos

---
+++ 001_variaveis.js
@@ -0,0 +1,13 @@
+let estudante = 'Eduardo Escudero';
+let professor = 'Ana Carolina';
+let curso = 'JavaScript para Backend';
+const escola = 'Alura'
+
+console.log(`
+    Curso: ${curso}
+    Professor: ${professor}
+    Aluno: ${estudante}
+    Escola: ${escola}
+`)
+
+// Obs: ao declarar uma variável, sempre utilize const a não ser que você tenha certeza que deverá alterar o valor da variável em algum momento, sendo assim, utilize let. 
