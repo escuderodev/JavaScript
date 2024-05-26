439b015a84c631e61cef17a09bdbd82ab24710b7
escuderodev <escudero.ti@gmail.com>
2024-05-26 11:59:43 -0300

    feat: escopo

---
+++ 002_escopo.js
@@ -0,0 +1,20 @@
+if(1 > 0) {
+    var estudante = 'Eduardo Escudero'
+    console.log(estudante)
+}
+
+console.log(estudante)
+
+if(1 > 0) {
+    let professor = 'Carolina Escudero'
+    console.log(professor)
+}
+
+// console.log(professor) não pode
+
+if(1 > 0) {
+    const curso = 'NodeJs'
+    console.log(curso)
+}
+
+// console.log(curso) não pode
