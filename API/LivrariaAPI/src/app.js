c032f50384c89ee8c2a2761ac0d754ebcb23afc5
escuderodev <escudero.ti@gmail.com>
2024-06-01 14:01:52 -0300

    feat: att

--- API/LivrariaAPI/src/app.js
+++ API/LivrariaAPI/src/app.js
@@ -16,6 +16,12 @@ const livros = [
     }
 ]
 
+function buscarLivroPorId(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Criando uma API com Node.js");
 });
@@ -24,9 +30,21 @@ app.get("/livros", (req, res) => {
     res.status(200).send(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscarLivroPorId(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
-    res.status(201)
+    res.status(201).send("Livro cadastrado com sucesso!");
 })
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscarLivroPorId(req.params.id);
+    livros[index].titulo = req.body.titulo
+    livros[index].autor = req.body.autor
+    res.status(200).json(livros[index]);
+});
 
 export default app;
