86dcb07bce1f83d654cb70d8ad298805335f8896
escuderodev <escudero.ti@gmail.com>
2024-06-01 12:58:22 -0300

    feat: att

--- API/LivrariaAPI/src/app.js
+++ API/LivrariaAPI/src/app.js
@@ -2,16 +2,25 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        titulo: "Casamento Blindado",
+        autor: "Renato Cardoso"
+    },
+    {
+        id: 2,
+        titulo: "Nos passos de Jesus",
+        autor: "Edir Macedo"
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Criando uma API com Node.js")
 });
 
 app.get("/livros", (req, res) => {
-    res.status(200).send("Vocẽ está na rota livros")
-});
-
-app.get("/autores", (req, res) => {
-    res.status(200).send("Você está na rota autores")
+    res.status(200).send(livros)
 });
 
 export default app;
