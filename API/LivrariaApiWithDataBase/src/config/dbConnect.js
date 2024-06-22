c754233506c2466e23dd279d02cba9a2624793c0
escuderodev <escudero.ti@gmail.com>
2024-06-22 17:11:35 -0300

    feat: att

--- API/LivrariaApiWithDataBase/src/config/dbConnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { mongo } from "mongoose"
-
-async function conectaDB() {
-    mongoose.connect(process.env.DB_STRING_CONNECTION);
-
-    return mongoose.connection;
-}
-
-export default conectaDB;
--- API/SolidAPI/src/repositories/IUsersRepository.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "../models/User";
-
-export interface IUsersRepository {
-    findUserByEmail(email: string): Promise<User>;
-    saveUser(user: User): Promise<void>;
-}
